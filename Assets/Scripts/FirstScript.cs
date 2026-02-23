1f5df1926a73ede0da09347ab71a68ff079763d0
abdarslan <abdarslan.9.8@gmail.com>
2026-02-24 01:03:51 +0300

    script FirstScript is attached to a cube and it applies force every frame with some variation according to game time

--- Assets/Scripts/FirstScript.cs
+++ Assets/Scripts/FirstScript.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class FirstScript : MonoBehaviour
 {
+    private Rigidbody rb;    
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        if (Time.time > 6 && Time.time < 8)
+        {
+            rb.AddForce(0, 0, 13);
+        }else
+        {
+
+            rb.AddForce(0, 10, 0);
+        }
     }
 }
